addfbe643ef17e06de196e3998aaa29c7f5857b2
Gregor Smith <gregor_smith@outlook.com>
2019-12-05 17:10:47 +0000

    typed logging level

--- torrentrss/logging.py
+++ torrentrss/logging.py
@@ -1,16 +1,27 @@
 from __future__ import annotations
 
 import sys
+from typing import Literal, Union
 from logging import Logger, StreamHandler, Formatter
 
 from .utils import wrap_for_asyncio
 from .constants import NAME, LOG_MESSAGE_FORMAT
 
 
+Level = Union[
+    Literal['DISABLE'],
+    Literal['DEBUG'],
+    Literal['INFO'],
+    Literal['WARNING'],
+    Literal['ERROR'],
+    Literal['CRITICAL']
+]
+
+
 _logger = Logger(name=NAME)
 
 
-def configure(level: str) -> None:
+def configure(level: Level) -> None:
     handler = StreamHandler(stream=sys.stdout)
     handler.setLevel(level)
     handler.setFormatter(Formatter(fmt=LOG_MESSAGE_FORMAT))
